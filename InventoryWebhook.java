b035a334dffdf7a6764d686267e2e03980ebc2c7
Saudah Atim <saudahatim@gmail.com>
2026-08-22 15:45:53 +0300

    Add complete inventory view

--- InventoryWebhook.java
+++ InventoryWebhook.java
@@ -16,6 +16,28 @@ class InventoryWebhook {
                 new InetSocketAddress("0.0.0.0", port), 0
         );
 
+        // Main page showing all products
+        server.createContext("/", (HttpExchange exchange) -> {
+
+            String response =
+                    "NORTHSTAR INVENTORY SYNC\n\n" +
+                    "LAP001 - Laptops\n" +
+                    "Available stock: " + WarehouseAPI.getStock("LAP001") + "\n\n" +
+                    "HED001 - Headphones\n" +
+                    "Available stock: " + WarehouseAPI.getStock("HED001") + "\n\n" +
+                    "MAK001 - Makeup Kits\n" +
+                    "Available stock: " + WarehouseAPI.getStock("MAK001") + "\n\n" +
+                    "BAR001 - Barbie Dolls\n" +
+                    "Available stock: " + WarehouseAPI.getStock("BAR001") + "\n";
+
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+
+            OutputStream output = exchange.getResponseBody();
+            output.write(response.getBytes());
+            output.close();
+        });
+
+        // Individual product stock lookup
         server.createContext("/stock", (HttpExchange exchange) -> {
 
             String path = exchange.getRequestURI().getPath();
@@ -59,6 +81,6 @@ class InventoryWebhook {
         server.start();
 
         System.out.println("Northstar Inventory Sync is LIVE!");
-        System.out.println("Open http://localhost:" + port + "/stock/LAP001");
+        System.out.println("Open http://localhost:" + port);
     }
 }
